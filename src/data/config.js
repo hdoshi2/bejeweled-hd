b2468b7b3b19183fdd8e1e1e48d948ab05e3f57e
Hardik Doshi <46202854+hdoshi2@users.noreply.github.com>
2022-04-13 14:37:24 -0600

    ADD configs and sampleboards files

---
+++ src/data/config.js
@@ -0,0 +1,12 @@
+export const colorScheme = [
+  "DarkViolet",
+  "orange",
+  "DarkCyan",
+  "red",
+  "yellow",
+  "navy",
+  "lightpink",
+];
+
+//Assign square dimensions
+export const width = 8;
---
+++ src/data/sampleBoards.js
@@ -0,0 +1,21 @@
+export const board_8x8_1 = [
+  ["DarkViolet", "navy", "red", "yellow", "DarkViolet", "red", "DarkViolet", "orange"],
+  ["DarkCyan", "DarkCyan", "yellow", "navy", "orange", "DarkCyan", "red", "DarkCyan"],
+  ["DarkCyan", "orange", "DarkCyan", "DarkCyan", "DarkCyan", "red", "red", "DarkCyan"],
+  ["navy", "orange", "yellow", "red", "DarkViolet", "red", "red", "red"],
+  ["DarkViolet", "DarkCyan", "red", "DarkCyan", "DarkViolet", "DarkViolet", "yellow", "DarkCyan"],
+  ["navy", "navy", "navy", "DarkCyan", "navy", "DarkViolet", "red", "navy"],
+  ["orange", "red", "navy", "yellow", "yellow", "DarkViolet", "DarkViolet", "DarkViolet"],
+  ["DarkViolet", "yellow", "DarkCyan", "navy", "navy", "DarkViolet", "DarkViolet", "orange"],
+];
+
+export const board_8x8_2 = [
+  ["DarkViolet", "yellow", "red", "yellow", "DarkViolet", "red", "DarkViolet", "orange"],
+  ["DarkCyan", "DarkCyan", "yellow", "navy", "orange", "DarkCyan", "red", "DarkCyan"],
+  ["DarkCyan", "orange", "DarkCyan", "DarkCyan", "DarkCyan", "red", "red", "DarkCyan"],
+  ["navy", "orange", "yellow", "red", "DarkViolet", "red", "red", "red"],
+  ["DarkViolet", "DarkCyan", "red", "DarkCyan", "DarkViolet", "DarkViolet", "yellow", "DarkCyan"],
+  ["navy", "navy", "navy", "DarkCyan", "navy", "DarkViolet", "red", "navy"],
+  ["orange", "red", "navy", "yellow", "yellow", "DarkViolet", "DarkViolet", "DarkViolet"],
+  ["DarkViolet", "yellow", "DarkCyan", "navy", "navy", "DarkViolet", "DarkViolet", "orange"],
+];
